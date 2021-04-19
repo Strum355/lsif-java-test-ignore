82abad25949604c9c2f352ce45cda4795cb7ec3f
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2021-04-19 13:40:10 +0100

    asdf

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -1,18 +1,33 @@
 package test;
 
 public class App {
-    public interface Banana {
-        String getGreeting();
-    }
+	public interface Banana {
+		String getGreeting();
+	}
 
-    public interface Test {
-        String getGreeting();
-    }
+	public interface Test {
+		String getGreeting();
+	}
 
-    public class Burger implements Test, Banana {
-        @Override
-        public String getGreeting() {
-            return "";
-        }
-    }
+	public class Burger implements Test, Banana {
+		@Override
+		public String getGreeting() {
+			return new CharSequence() {
+				@Override
+				public int length() {
+					return 0;
+				}
+
+				@Override
+				public char charAt(int index) {
+					return 0;
+				}
+
+				@Override
+				public CharSequence subSequence(int start, int end) {
+					return null;
+				}
+			}.toString();
+		}
+	}
 }
